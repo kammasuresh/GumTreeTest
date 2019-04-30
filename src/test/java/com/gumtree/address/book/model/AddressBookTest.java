9c3473277888314730af6e43a1c8c6ad7f201137
kammasuresh <kamma.suresh99@gmail.com>
2019-04-30 23:56:01 +0100

    added test case for GetName

--- src/test/java/com/gumtree/address/book/model/AddressBookTest.java
+++ src/test/java/com/gumtree/address/book/model/AddressBookTest.java
@@ -1,7 +1,26 @@
 package com.gumtree.address.book.model;
 
-public class AddressBookTest {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
-    
+import org.junit.Before;
+
+public class AddressBookTest {
+	 private AddressBook addressBook;
+	 private final static String DATE_FORMAT = "dd/MM/yyyy";
+
+	    @Before
+	    public void setUp() throws Exception {
+	        DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH);
+	        this.addressBook = new AddressBook();
+	        addressBook.setName("Fred");
+	        addressBook.setGender("Male");
+	        addressBook.setBirthDate(dateFormat.parse("14/08/1974"));
+	    }
+	    @Test
+	    public void testGetName() throws Exception {
+	        assertEquals("Fred", addressBook.getName());
+	    }
 
 }
