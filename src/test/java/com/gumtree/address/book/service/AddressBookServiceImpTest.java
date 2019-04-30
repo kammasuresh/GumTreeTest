99d1753685a6ba4e0d49ad2f05550644e005ba19
kammasuresh <kamma.suresh99@gmail.com>
2019-05-01 00:13:54 +0100

    added getAddressBookList method  unit test case, in the implmentation was failed  date was mismatching, so decided toi create helper class for date conversion

--- src/test/java/com/gumtree/address/book/service/AddressBookServiceImpTest.java
+++ src/test/java/com/gumtree/address/book/service/AddressBookServiceImpTest.java
@@ -1,5 +1,25 @@
 package com.gumtree.address.book.service;
 
-public class AddressBookServiceImpTest {
+import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.gumtree.address.book.model.AddressBook;
+
+public class AddressBookServiceImpTest {
+	private AddressBookServiceImpl addressBookService;
+
+    @Before
+    public void setUp() throws Exception {
+        this.addressBookService = new AddressBookServiceImpl();
+    }
+    @Test
+    public void testGetAddressBookList() throws Exception {
+        List<AddressBook> addressBooks = addressBookService.getAddressBookList();
+        assertEquals(5, addressBooks.size());
+    }
+	
 }
