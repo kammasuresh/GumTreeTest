23cc95c32cc05e880e4c85ac7b9bc1c16dc87aad
kammasuresh <kamma.suresh99@gmail.com>
2019-05-01 00:42:33 +0100

    // How many males are in the address book?    related test case created and failed  in unit test case  and developed  to pass test case

--- src/test/java/com/gumtree/address/book/userStoryValidation/AddressBookUserStoryValidationTest.java
+++ src/test/java/com/gumtree/address/book/userStoryValidation/AddressBookUserStoryValidationTest.java
@@ -1,12 +1,26 @@
 package com.gumtree.address.book.userStoryValidation;
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class AddressBookUserStoryValidationTest {
 	 private AddressBookUserStoryValidation addressBookUserStoryValidation;
 
 	    @Before
 	    public void setUp() throws Exception {
-	        this.AddressBookUserStoryValidation = new AddressBookUserStoryValidation();
+	        this.addressBookUserStoryValidation = new AddressBookUserStoryValidation();
+	    }
+	    /*Your application needs to read the attached AddressBook file and answer the following questions:
+     How many males are in the address book?
+     Who is the oldest person in the address book?
+     How many days older is Bill than Paul?*/
+	    //
+	    @Test
+	    public void testGetHowManyMaleGender() throws Exception {
+	       // How many males are in the address book?
+
+	        Gender gender = Gender.MALE;
+	        assertEquals(3, addressBookUserStoryValidation.getHowManyGender(gender.getValue()));
+	        assertEquals(0, addressBookUserStoryValidation.getHowManyGender("boo"));
 	    }
 }
