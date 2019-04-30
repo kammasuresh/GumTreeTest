3c094cb1936c813d5ef3462959c40d33e8c3187d
kammasuresh <kamma.suresh99@gmail.com>
2019-05-01 00:33:52 +0100

    test case failed and created  new test classes

---
+++ src/test/java/com/gumtree/address/book/userStoryValidation/AddressBookUserStoryValidationTest.java
@@ -0,0 +1,12 @@
+package com.gumtree.address.book.userStoryValidation;
+
+import org.junit.Before;
+
+public class AddressBookUserStoryValidationTest {
+	 private AddressBookUserStoryValidation addressBookUserStoryValidation;
+
+	    @Before
+	    public void setUp() throws Exception {
+	        this.AddressBookUserStoryValidation = new AddressBookUserStoryValidation();
+	    }
+}
